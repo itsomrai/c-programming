2ca7ca571a827d3aa6b5147276b3bb2a599cd945
itsomrai <work.omrai@gmail.com>
2026-05-24 22:35:42 +0530

    day 20 of c progrmming

---
+++ day20-arrays/pushZeoToEnd.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void pushZer0ToEnd(int *arr, int n){
+    int temp[n];
+    int j=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0)
+           temp[j++]=arr[i];
+        
+    }
+    while(j<n)
+        temp[j++]=0;
+
+        for(int i=0;i<n;i++){
+            arr[i]=temp[i];
+        }
+}
+int main()
+{
+int arr[6]={1,2,0,0,4,5};
+int n=sizeof(arr)/sizeof(arr[0]);
+
+pushZer0ToEnd(arr,n);
+for (int i = 0; i < n; i++)
+{
+    printf("%d",arr[i]);
+}
+
+return 0;
+}
